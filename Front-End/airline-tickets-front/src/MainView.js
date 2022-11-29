ef1ac2049c97d5cefede0282ef349acd82cfe3f7
christenis98 <canei01@ucm.es>
2022-11-29 12:57:50 +0100

    MainView

---
+++ Front-End/airline-tickets-front/src/MainView.js
@@ -0,0 +1,14 @@
+import Origins from "./Components/Origins.js";
+import Container from "react-bootstrap/Container";
+
+function MainView() {
+  return (
+    <Container className="parent container d-flex justify-content-center align-items-center h-100">
+      <Container className="row justify-content-center ">
+        <Origins />
+      </Container>
+    </Container>
+  );
+}
+
+export default MainView;
